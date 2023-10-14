602435704fcec3b3cfbd065383548077c10b3e2b
Salma-Raf <rksalma6@gmail.com>
2023-10-14 16:56:45 +0100

    first commit

---
+++ src/app/gestion-semester/components/semestres/semestres.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-semestres',
+  templateUrl: './semestres.component.html',
+  styleUrls: ['./semestres.component.scss']
+})
+export class SemestresComponent {
+
+}
---
+++ src/app/gestion-semester/gestion-semester.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-semester',
+  templateUrl: './gestion-semester.component.html',
+  styleUrls: ['./gestion-semester.component.scss']
+})
+export class GestionSemesterComponent {
+
+}
